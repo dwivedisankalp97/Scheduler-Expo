183dbb9ddbe1344b39b35d0035ade26d4004aa9e
Sankalp Dwivedi <dwivedisankalp97>
2020-09-22 22:08:09 -0700

    Fetch Data

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, TouchableOpacity, SafeAreaView, ScrollView} from 'react-native';
 import CourseList from './components/CourseList'
 
@@ -29,18 +29,34 @@ const schedule = {
 };
 
 
+
+
 const Banner = ({title}) => (
-    <Text style={styles.bannerStyle}>{title}</Text>
+    <Text style={styles.bannerStyle}>{title || 'Loading'}</Text>
 );
 
 const App = () => {
+    const [schedule, setSchedule] = useState({ title: '', courses: [] });
+
+    const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+    useEffect(() => {
+        const fetchSchedule =  async () => {
+            const response = await fetch(url);
+            if (!response.ok) throw response;
+            const json = await response.json();
+            setSchedule(json);
+        };
+        fetchSchedule();
+    }, []);
+
     return (
         <SafeAreaView style={styles.container}>
         <Banner title={schedule.title} />
         <CourseList courses={schedule.courses} />
         </SafeAreaView>
     );
-}
+};
 
 
 const styles = StyleSheet.create({
--- components/CourseList.js
+++ components/CourseList.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         flexWrap: 'wrap',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
     },
 })
 
